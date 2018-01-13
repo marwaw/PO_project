915cd0d5f07fe9b4d53e61c9d1df23ad09696514
marwaw <martyna.waw96@gmail.com>
2018-01-13 13:03:42 +0100

    Requirements and more register

--- dyplomowanie/admin.py
+++ dyplomowanie/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .model.models import Deklaracja, FormaStudiow
+from .model.models import Deklaracja, FormaStudiow, Nauczycielakademicki
 
 class DeklaracajAdmin(admin.ModelAdmin):
     pass
@@ -8,5 +8,9 @@ class DeklaracajAdmin(admin.ModelAdmin):
 class FormaStudiowAdmin(admin.ModelAdmin):
     pass
 
+class NauczycielakademickiAdmin(admin.ModelAdmin):
+    pass
+
 admin.register(Deklaracja, DeklaracajAdmin)
 admin.register(FormaStudiow, FormaStudiowAdmin)
+admin.register(Nauczycielakademicki, NauczycielakademickiAdmin)
