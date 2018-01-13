4f05a1a0976096d642b6b73a5a0459800a7a854f
marwaw <martyna.waw96@gmail.com>
2018-01-13 15:38:21 +0100

    Admin and register models

--- dyplomowanie/admin.py
+++ dyplomowanie/admin.py
@@ -1,16 +1,47 @@
 from django.contrib import admin
-from .model.models import Deklaracja, FormaStudiow, Nauczycielakademicki
+from .model import Deklaracja
+from .model import FormaStudiow
+from .model import JezykRealizacji
+from .model import NauczycielAkademicki
+from .model import PowiadomienieOWyborzeTematu
+from .model import StopienStudiow
+from .model import Student
+from .model import Temat
+from .model import TypPracy
 
-class DeklaracajAdmin(admin.ModelAdmin):
+class DeklaracjaAdmin(admin.ModelAdmin):
     pass
-
 
 class FormaStudiowAdmin(admin.ModelAdmin):
     pass
 
-class NauczycielakademickiAdmin(admin.ModelAdmin):
+class JezykRealizacjiAdmin(admin.ModelAdmin):
     pass
 
-admin.register(Deklaracja, DeklaracajAdmin)
+class NauczycielAkademickiAdmin(admin.ModelAdmin):
+    pass
+
+class PowiadomienieOWyborzeTematuAdmin(admin.ModelAdmin):
+    pass
+
+class StopienStudiowAdmin(admin.ModelAdmin):
+    pass
+
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+class TematAdmin(admin.ModelAdmin):
+    pass
+
+class TypPracyAdmin(admin.ModelAdmin):
+    pass
+
+admin.register(Deklaracja, DeklaracjaAdmin)
 admin.register(FormaStudiow, FormaStudiowAdmin)
-admin.register(Nauczycielakademicki, NauczycielakademickiAdmin)
+admin.register(JezykRealizacji, JezykRealizacjiAdmin)
+admin.register(NauczycielAkademicki, NauczycielAkademickiAdmin)
+admin.register(PowiadomienieOWyborzeTematu, PowiadomienieOWyborzeTematuAdmin)
+admin.register(StopienStudiow, StopienStudiowAdmin)
+admin.register(Student, StudentAdmin)
+admin.register(Temat, TematAdmin)
+admin.register(TypPracy, TypPracyAdmin)
