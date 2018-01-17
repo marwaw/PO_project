8435019a152446db02d6bbbb27935282571df3db
marwaw <martyna.waw96@gmail.com>
2018-01-17 21:01:30 +0100

    Base site

--- dyplomowanie/kontroler/views.py
+++ dyplomowanie/kontroler/views.py
@@ -1,7 +1,6 @@
-from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
-import datetime
+
 from django.views.generic import View
 from django.urls import reverse
 
@@ -20,19 +19,8 @@ def hello_world(request):
     a = 'heeeej'
     return HttpResponse(a)
 
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>Teraz: %s.</body></html>" %now
-    return HttpResponse(html)
-
-def hours_ahead(request, offset):
-    try:
-        offset = int(offset)
-    except ValueError:
-        raise Http404()
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>Za %s godz, będzie: %s.</body></html>" %(offset, dt)
-
-    return HttpResponse(html)
+def index(request):
+    context = { 'user': 'Jan Kowalski'}
+    return render(request, 'base.html', context)
 
 
