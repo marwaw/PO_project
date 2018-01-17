8cc90a310ff4755dbde45e387ad8463c8c555f9b
marwaw <martyna.waw96@gmail.com>
2018-01-17 21:01:10 +0100

    Changed path to the templates directory

--- po_project/settings.py
+++ po_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'po_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'dyplomowanie/widok')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
