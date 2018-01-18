3944547dfdf9b249561c0b8c95947ce90292be4d
marwaw <martyna.waw96@gmail.com>
2018-01-18 18:30:51 +0100

    Another new sites

--- dyplomowanie/kontroler/views.py
+++ dyplomowanie/kontroler/views.py
@@ -13,11 +13,21 @@ class Topics(View):
         context = {'user': 'Jan Kowalski'}
         return render(request, 'tematy/listy_tematow.html', context)
 
+class Topics_Options(View):
+    def get(self, request):
+        context = {'user': 'Jan Kowalski'}
+        return render(request, 'tematy/opcje_przegladania.html', context)
+
 
 class Work(View):
     def get(self, request):
         context = {'user': 'Jan Kowalski'}
         return render(request, 'moja_praca.html', context)
+
+class Declaration(View):
+    def get(self, request):
+        context = {'user': 'Jan Kowalski'}
+        return render(request, 'deklaracja/deklaracja.html', context)
 
 
 
--- dyplomowanie/urls.py
+++ dyplomowanie/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from dyplomowanie.kontroler.views import Base
 from dyplomowanie.kontroler.views import Topics
+from dyplomowanie.kontroler.views import Topics_Options
 from dyplomowanie.kontroler.views import Work
+from dyplomowanie.kontroler.views import Declaration
 
 urlpatterns = [
     path('index', Base.as_view(), name = "base"),
-    path('przegladanie_list', Topics.as_view(), name = "topics"),
-    path('moja_praca', Work.as_view(), name = "work")
+    path('tematy/przegladaj', Topics.as_view(), name = "topics"),
+    path('tematy', Topics_Options.as_view(), name = "topics_options"),
+    path('moja_praca/deklaracja', Declaration.as_view(), name = "declaration"),
+    path('moja_praca', Work.as_view(), name = "work"),
+    path('', Base.as_view(), name = "base")
 ]
