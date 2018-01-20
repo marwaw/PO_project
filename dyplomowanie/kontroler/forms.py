d5a96387ebe5a898cc63cd3ffb73d189254b24aa
marwaw <martyna.waw96@gmail.com>
2018-01-20 17:34:38 +0100

    Choices from db

--- dyplomowanie/kontroler/forms.py
+++ dyplomowanie/kontroler/forms.py
@@ -1,16 +1,18 @@
 from django import forms
 from django.forms import ModelForm
-
+from dyplomowanie.model.JezykRealizacji import JezykRealizacji
 from dyplomowanie.model import Deklaracja
 
 
-class NameForm(forms.Form):
+class OptionsForm(forms.Form):
     TOPIC_CHOICES = [('all', 'wszystkie'), ('free', 'tylko wolne')]
     name = forms.CharField(label = 'Nazwisko promotora', max_length=100, required=False)
     topic_options = forms.CharField(label = 'Jakie tematy wyświetlić?',
-                                    widget = forms.RadioSelect(choices = TOPIC_CHOICES))
+                                    widget = forms.RadioSelect(choices = TOPIC_CHOICES), initial='', required=False)
 
 class DeclarationForm2(forms.Form):
+    jezyki = [(jezyk.id, jezyk.nazwajezyka) for jezyk in JezykRealizacji.objects.all()]
+
     name = forms.CharField(label='Imię')
     surname = forms.CharField(label='Nazwisko')
     subject = forms.CharField(label='Kierunek')
@@ -19,6 +21,6 @@ class DeclarationForm2(forms.Form):
     topic_PL = forms.CharField(label='Temat PL')
     topic_ENG = forms.CharField(label='Temat ENG')
     supervisor = forms.CharField(label='Opiekun')
-    language = forms.ChoiceField(label='Język', choices = [('polish', 'polski'), ('english', 'angielski')])
+    language = forms.ChoiceField(label='Język', choices = jezyki, initial='polski')
     goal = forms.CharField(label = 'Cel i zakres pracy dyplomowej', widget = forms.Textarea, max_length= 2000)
     description = forms.CharField(label = 'Opis pracy', widget = forms.Textarea, max_length= 2000)
