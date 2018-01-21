c567638f811e27b62bfbadf8abc0207d1e330f58
marwaw <martyna.waw96@gmail.com>
2018-01-21 11:43:31 +0100

    Info about student from db

--- dyplomowanie/kontroler/deklaracja.py
+++ dyplomowanie/kontroler/deklaracja.py
@@ -2,36 +2,39 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from dyplomowanie.model.Student import Student
 from .forms import DeclarationForm2
 
 class Declaration(View):
     def get(self, request):
+        student = Student.objects.get(id = 2)
 
-        student_info = \
-            {'name': 'Jan',
-             'surname': 'Kowalski',
-             'subject': 'Informatyka',
-             'speciality' : '-',
-             'studies_more': 'I stopień, studia dzienne, III rok',
-             'topic_PL': 'Temat temat',
-             'topic_ENG': 'Topic topic',
-             'supervisor': 'Adamczyk Adam'}
+        student_info = {
+            'name': student.imie,
+            'surname': student.nazwisko,
+            'subject': student.kierunekstudiow,
+            'speciality' : student.specjalnosc,
+            'studies_more': f'{student.stopienstudiow} stopień, studia {student.formastudiow}, {student.rokstudiow} rok',
+            'topic_PL': student.tematid.trescpl,
+            'topic_ENG': student.tematid.tresceng,
+            'supervisor': f'{student.tematid.nauczycielakademickiid.imie} {student.tematid.nauczycielakademickiid.nazwisko}' }
 
         form = DeclarationForm2(student_info)
-        context = {'user': 'Jan Kowalski', 'form': form}
+        context = {'user': f'{student.imie} {student.nazwisko}', 'form': form}
         return render(request, 'deklaracja/deklaracja.html', context)
 
     def post(self, request):
-        student_info = \
-            {'name': 'Jan',
-             'surname': 'Kowalski',
-             'subject': 'Informatyka',
-             'speciality': '-',
-             'studies_more': 'I stopień, studia dzienne, III rok',
-             'topic_PL': 'Temat temat',
-             'topic_ENG': 'Topic topic',
-             'supervisor': 'Adamczyk Adam'}
+        student = Student.objects.get(id=2)
 
+        student_info = {
+            'name': student.imie,
+            'surname': student.nazwisko,
+            'subject': student.kierunekstudiow,
+            'speciality': student.specjalnosc,
+            'studies_more': f'{student.stopienstudiow} stopień, studia {student.formastudiow}, {student.rokstudiow} rok',
+            'topic_PL': student.tematid.trescpl,
+            'topic_ENG': student.tematid.tresceng,
+            'supervisor': f'{student.tematid.nauczycielakademickiid.imie} {student.tematid.nauczycielakademickiid.nazwisko}'}
         form = DeclarationForm2(request.POST)
         if form.is_valid():
-            return HttpResponse(form.cleaned_data['opis'])
+            return HttpResponse(form.cleaned_data['description'])
