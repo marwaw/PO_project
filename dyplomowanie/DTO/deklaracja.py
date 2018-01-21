b633ba5c75cf4a44488d7c79f8b9b6d555551eb2
marwaw <martyna.waw96@gmail.com>
2018-01-21 11:40:57 +0100

    Default value

--- dyplomowanie/DTO/deklaracja.py
+++
@@ -1,7 +0,0 @@
-class StudentDTO(object):
-
-    def __init__(self, id, tresc_pl, promotor, status):
-        self.id = id
-        self.tresc_pl = tresc_pl
-        self.promotor = promotor
-        self.status = status
--- dyplomowanie/DTO/tematy.py
+++ dyplomowanie/DTO/tematy.py
@@ -1,6 +1,6 @@
 class TematDTO(object):
 
-    def __init__(self, id, tresc_pl, promotor, status):
+    def __init__(self, id, tresc_pl, promotor, status = None):
         self.id = id
         self.tresc_pl = tresc_pl
         self.promotor = promotor
