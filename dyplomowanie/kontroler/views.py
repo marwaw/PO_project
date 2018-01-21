49c500b64d225057b40d0959fb2f2de04e6607a4
marwaw <martyna.waw96@gmail.com>
2018-01-21 11:44:57 +0100

    Info about student's work

--- dyplomowanie/kontroler/views.py
+++ dyplomowanie/kontroler/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 
 from dyplomowanie.model.Deklaracja import DeclarationForm
+from dyplomowanie.model.Student import Student
 from .forms import OptionsForm, DeclarationForm2
 from dyplomowanie.model.Temat import Temat
 from dyplomowanie.DTO.tematy import TematDTO
@@ -11,13 +12,21 @@ from dyplomowanie.DTO.tematy import TematDTO
 
 class Base(View):
     def get(self, request):
-        context = {'user': 'Jan Kowalski'}
+        student = Student.objects.get(id=2)
+
+        context = {f'{student.imie} {student.nazwisko}'}
         return render(request, 'base.html', context)
 
 
 class Work(View):
     def get(self, request):
-        context = {'user': 'Jan Kowalski'}
+        student = Student.objects.get(id=2)
+        temat = None
+        if student.tematid:
+            temat = Temat.objects.get(id = student.tematid.id)
+            temat = TematDTO(temat.id, temat.trescpl, str(temat.nauczycielakademickiid))
+
+        context = {'user': f'{student.imie} {student.nazwisko}', 'temat': temat}
         return render(request, 'moja_praca.html', context)
 
 
